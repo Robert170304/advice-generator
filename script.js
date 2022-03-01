de201e92de7caebc251229e9dc4ef439848ef86f
Robret Macwan <96678518+Robert170304@users.noreply.github.com>
2022-03-01 17:27:52 +0530

    update script commit

--- script.js
+++ script.js
@@ -5,21 +5,6 @@ const adviceNumber = document.querySelector(".advice-num");
 const adviceText = document.querySelector(".advice");
 const apiUrl = "https://api.adviceslip.com/advice";
 
-//Button click event
-/*diceBtn.addEventListener("click", () => {
-  //Request Data
-  fetch(apiUrl, {cache: "no-cache"})
-    .then((response) => response.json())
-    .then((response) => {
-      let data = response.slip;
-      let dataId = data.id;
-      let dataAdvice = data.advice;
-      //Inject to DOM
-      adviceNumber.innerHTML = `advice # ${dataId}`;
-      adviceText.innerHTML = dataAdvice;
-    });
-}); */
-
 diceBtn.addEventListener('click', function() {
     // Request data
     fetch(apiUrl, {cache: "no-cache"})
